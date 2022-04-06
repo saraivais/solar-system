2a0c0e8a73bbbc28d30b8974074483a4424352fe
saraivais <isadorasaraiva95@gmail.com>
2022-04-06 13:31:23 -0300

    Feat: Header Component~ Adiciona componente header com h1 e titulo~ classes adicionadas

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="header-title">Sistema Solar</h1>
+      </header>);
+  }
+}
+
+export default Header;
