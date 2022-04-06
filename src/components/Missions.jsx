2077ea04b4de7f500f8c58ce3797adb1a8744172
saraivais <isadorasaraiva95@gmail.com>
2022-04-06 13:39:50 -0300

    Feat: Missions Component~ Utiliza Title, MissionCards e importa missions.js de data~ Utiliza map para adicionar missoes

---
+++ src/components/Missions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
+import '../App.css';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        <section className="all-missions">
+          {missions.map((mission) => (<MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />))}
+        </section>
+      </div>);
+  }
+}
+
+export default Missions;
